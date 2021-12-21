bdf5768fe79f037feccd23cccbd1436e19dc0413
sionhu <hu440@purdue.edu>
2021-12-20 19:19:12 -1000

    update documents

--- honeypeartea/test.py
+++ honeypeartea/test.py
@@ -22,15 +22,27 @@ def main():
             'African American': 0,
             'American Indian': 0
         }
-
         for row in data:
             for race in dict.keys():
                 if row[2] == college and row[3] == race and row[7] == str(year):
                     dict[race] += float(row[6])
-
-        reminder = 100 - sum(dict.values())
-        print(reminder)
-
+        # Round up to 100
+        value_sum = sum(dict.values())
+        dict['Asian'] += (100-value_sum)/2
+        dict['White'] += (100-value_sum)/2
         pprint.pprint(dict)
+
+    elif category == 'gpa':
+        data = csv2list('../static/gpa.csv')
+        for row in data:
+            if row[0] == str(year) and row[1] == college:
+                print(f' - Found it! GPA: {row[2]}')
+                return row[2]
+
+    elif category == 'sat':
+        data = csv2list('../static/sat.csv')
+        for row in data:
+            
+
 if __name__ == '__main__':
     main()
